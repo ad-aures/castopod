ce5934419a516c9926dd3fd0ace3c11a95b60722
Benjamin Bellamy <ben@podlibre.org>
2020-06-08 16:25:23 +0200

    feat: add platforms tables

---
+++ app/Database/Migrations/2020-06-05-190000_add_platforms.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Class AddPlatforms
+ * Creates platforms table in database
+ * @author     Benjamin Bellamy <ben@podlibre.org>
+ * @copyright  2020 Podlibre
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html AGPL3
+ * @link       https://castopod.org/
+ */
+namespace App\Database\Migrations;
+
+use \CodeIgniter\Database\Migration;
+
+class AddPlatforms extends Migration
+{
+
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'BIGINT',
+                'constraint' => 20,
+                'unsigned' => true,
+                'auto_increment' => true,
+                'comment' => 'The platform ID',
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 191,
+                'unique' => true,
+                'comment' => 'Platform name.',
+            ],
+            'home_url' => [
+                'type' => 'VARCHAR',
+                'constraint' => 191,
+                'comment' => 'Platform home URL.',
+            ],
+            'submit_url' => [
+                'type' => 'VARCHAR',
+                'constraint' => 191,
+                'comment' => 'Platform URL to submit podcasts.',
+                'null' => true,
+            ],
+            'iosapp_url' => [
+                'type' => 'VARCHAR',
+                'constraint' => 191,
+                'comment' => 'Platform iOS app URL (if any).',
+                'null' => true,
+            ],
+            'androidapp_url' => [
+                'type' => 'VARCHAR',
+                'constraint' => 191,
+                'comment' => 'Platform Android app URL (if any).',
+                'null' => true,
+            ],
+            'comment' => [
+                'type' => 'TEXT',
+                'comment' => 'Comment.',
+                'null' => true,
+            ],
+            'display_by_default' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+                'default' => 0,
+                'comment' => 'True if the platform link should be displayed by default.',
+            ],
+            'ios_deeplink' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+                'default' => 0,
+                'comment' => 'iOS deeplinking for this platform.',
+            ],
+            'android_deeplink' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+                'default' => 0,
+                'comment' => 'Android deeplinking for this platform: 0=No, 1=Manual, 2=Automatic.',
+            ],
+            'logo_file_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 1024,
+                'comment' => 'The logo for this platform.',
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('platforms');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('platforms');
+    }
+}
---
+++ app/Database/Migrations/2020-06-08-160000_add_platformlinks.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Class AddPlatformsLinks
+ * Creates platformlinks table in database
+ * @author     Benjamin Bellamy <ben@podlibre.org>
+ * @copyright  2020 Podlibre
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html AGPL3
+ * @link       https://castopod.org/
+ */
+namespace App\Database\Migrations;
+
+use \CodeIgniter\Database\Migration;
+
+class AddPlatformLinks extends Migration
+{
+
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'BIGINT',
+                'constraint' => 20,
+                'unsigned' => true,
+                'auto_increment' => true,
+                'comment' => 'The link ID',
+            ],
+            'podcast_id' => [
+                'type' => 'BIGINT',
+                'constraint' => 20,
+                'unsigned' => true,
+                'comment' => 'The podcast ID',
+            ],
+            'platform_id' => [
+                'type' => 'BIGINT',
+                'constraint' => 20,
+                'unsigned' => true,
+                'comment' => 'The platform ID',
+            ],
+            'link_url' => [
+                'type' => 'VARCHAR',
+                'constraint' => 191,
+                'comment' => 'Podcast link URL on this platform.',
+            ],
+            'comment' => [
+                'type' => 'TEXT',
+                'comment' => 'Comment.',
+                'null' => true,
+            ],
+            'visible' => [
+                'type' => 'TINYINT',
+                'constraint' => 1,
+                'default' => 0,
+                'comment' => 'Show this link.',
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('podcast_id', 'podcasts', 'id');
+        $this->forge->addForeignKey('platform_id', 'platforms', 'id');
+        $this->forge->createTable('platform_links');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('platform_links');
+    }
+}
