1c4a50442bea2d3449efce9c5ff1c80743152f55
Yassine Doghri <yassine@doghri.fr>
2020-10-08 18:06:30 +0000

    feat: set podcast / episode description in the pages description meta tag
    strip html tags from description_html attributes to generate text only description
    
    closes #44

--- app/Views/episode.php
+++ app/Views/episode.php
@@ -5,7 +5,9 @@
 <head>
     <meta charset="UTF-8"/>
     <title><?= $episode->title ?></title>
-    <meta name="description" content="Castopod is an open-source hosting platform made for podcasters who want engage and interact with their audience."/>
+    <meta name="description" content="<?= strip_tags(
+        $episode->description_html
+    ) ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
     <link rel="stylesheet" href="/assets/index.css"/>
--- app/Views/podcast.php
+++ app/Views/podcast.php
@@ -6,7 +6,9 @@
 <head>
     <meta charset="UTF-8"/>
     <title><?= $podcast->title ?></title>
-    <meta name="description" content="Castopod is an open-source hosting platform made for podcasters who want engage and interact with their audience."/>
+    <meta name="description" content="<?= strip_tags(
+        $podcast->description_html
+    ) ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
     <link rel="stylesheet" href="/assets/index.css"/>
