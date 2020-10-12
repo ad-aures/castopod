6238a43863210afe8988ad7cf251e6bfc6c8557c
Yassine Doghri <yassine@doghri.fr>
2020-10-12 19:21:50 +0000

    fix: escape generated feed tag values and remove new lines from public pages meta description
    fixes #57, #46

--- app/Libraries/SimpleRSSElement.php
+++ app/Libraries/SimpleRSSElement.php
@@ -14,17 +14,40 @@ class SimpleRSSElement extends SimpleXMLElement
 {
     /**
      * Adds a child with $value inside CDATA
-     * @param unknown $name
-     * @param unknown $value
+     *
+     * @param string $name — The name of the child element to add.
+     * @param string $value — [optional] If specified, the value of the child element.
+     * @param string $namespace [optional] If specified, the namespace to which the child element belongs.
      */
     public function addChildWithCDATA($name, $value = null, $namespace = null)
     {
-        $new_child = $this->addChild($name, null, $namespace);
+        $new_child = parent::addChild($name, null, $namespace);
 
         if ($new_child !== null) {
             $node = dom_import_simplexml($new_child);
             $no = $node->ownerDocument;
             $node->appendChild($no->createCDATASection($value));
+        }
+
+        return $new_child;
+    }
+
+    /**
+     * Adds a child element to the XML node with escaped $value if specified.
+     * Override of addChild method as SimpleXMLElement's addChild method doesn't escape ampersand
+     *
+     * @param string $name — The name of the child element to add.
+     * @param string $value — [optional] If specified, the value of the child element.
+     * @param string $namespace [optional] If specified, the namespace to which the child element belongs.
+     */
+    public function addChild($name, $value = null, $namespace = null)
+    {
+        $new_child = parent::addChild($name, null, $namespace);
+
+        if ($new_child !== null) {
+            $node = dom_import_simplexml($new_child);
+            $no = $node->ownerDocument;
+            $node->appendChild($no->createTextNode(esc($value)));
         }
 
         return $new_child;
--- app/Views/podcast.php
+++ app/Views/podcast.php
@@ -6,8 +6,8 @@
 <head>
     <meta charset="UTF-8"/>
     <title><?= $podcast->title ?></title>
-    <meta name="description" content="<?= strip_tags(
-        $podcast->description_html
+    <meta name="description" content="<?= trim(
+        preg_replace('/\s+/', ' ', strip_tags($podcast->description_html))
     ) ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
