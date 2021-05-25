89ac4979f2f33808399961337ba66c27aed95adb
Yassine Doghri <yassine@doghri.fr>
2021-05-25 09:02:23 +0000

    ci: increase artifacts expire_in for dependencies + reorganise bundle & release jobs

--- scripts/bundle-prepare.sh
+++ scripts/bundle-prepare.sh
@@ -3,5 +3,5 @@
 # install only production dependencies using the --no-dev option
 php composer.phar install --no-dev --prefer-dist --no-ansi --no-interaction --no-progress --ignore-platform-reqs
 
-# build all production UI assets
+# build all production static assets (css, js, images, icons, fonts, etc.)
 npm run build
