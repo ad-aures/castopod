7b8494d2b3ccf68a21c1f7f2b69f181f9c5051d0
Yassine Doghri <yassine@doghri.fr>
2021-06-23 14:53:31 +0000

    ci: update lint-commit-msg.sh script

--- scripts/lint-commit-msg.sh
+++ scripts/lint-commit-msg.sh
@@ -2,11 +2,17 @@
 
 # see https://github.com/conventional-changelog/commitlint/issues/885
 
-if [ "${CI_COMMIT_BEFORE_SHA}" = "0000000000000000000000000000000000000000" ]
+if [ "${CI_COMMIT_BEFORE_SHA}" = "0000000000000000000000000000000000000000" ];
 then
     echo "commitlint from HEAD^"
     npx commitlint --from=HEAD^
 else
     echo "commitlint from ${CI_COMMIT_BEFORE_SHA}"
-    npx commitlint --from="${CI_COMMIT_BEFORE_SHA}"
+    br=`git branch -r --contains ${CI_COMMIT_BEFORE_SHA}`
+    if [ ! -n $br ];
+    then 
+        npx commitlint --from=HEAD^
+    else
+        npx commitlint --from="${CI_COMMIT_BEFORE_SHA}"
+    fi
 fi
