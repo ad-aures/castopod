8208ab6785aae8c49f78eb9ac8cd53d77ec8e5e5
Yassine Doghri <yassine@doghri.fr>
2021-10-22 13:11:33 +0000

    feat: style file inputs using tailwind's file class

--- app/Views/Components/Forms/Input.php
+++ app/Views/Components/Forms/Input.php
@@ -10,9 +10,15 @@ class Input extends FormComponent
 
     public function render(): string
     {
-        $class = 'px-3 py-2 bg-white border-black rounded-lg focus:border-black border-3 focus:ring-castopod ' . $this->class;
+        $baseClass = 'bg-white border-black rounded-lg focus:border-black border-3 focus:ring-castopod focus-within:ring-castopod ' . $this->class;
 
-        $this->attributes['class'] = $class;
+        $this->attributes['class'] = $baseClass;
+
+        if ($this->type === 'file') {
+            $this->attributes['class'] .= ' file:px-3 file:py-2 file:h-[40px] file:font-semibold file:text-gray-800 file:text-sm file:rounded-none file:border-none file:bg-gray-200 file:hover:bg-gray-300 file:cursor-pointer';
+        } else {
+            $this->attributes['class'] .= ' px-3 py-2';
+        }
 
         if ($this->required) {
             $this->attributes['required'] = 'required';
--- themes/cp_app/episode/comment.php
+++ themes/cp_app/episode/comment.php
@@ -19,7 +19,7 @@
 
 <?= $this->section('content') ?>
 <div class="max-w-2xl px-6 mx-auto">
-    <nav class="py-3">
+    <nav class="mb-2">
         <a href="<?= route_to('episode', $podcast->handle, $episode->slug) ?>"
         class="inline-flex items-center px-4 py-2 text-sm focus:ring-castopod"><?= icon(
         'arrow-left',
