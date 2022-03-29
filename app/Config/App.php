c220b310ed59cad188af044b1fed0c39efc7da5b
Yassine Doghri <yassine@doghri.fr>
2022-03-29 15:33:32 +0000

    feat(i18n): add german language as supported locale + create Language files from english source

--- app/Config/App.php
+++ app/Config/App.php
@@ -101,7 +101,7 @@ class App extends BaseConfig
      *
      * @var string[]
      */
-    public array $supportedLocales = ['en', 'fr', 'pl'];
+    public array $supportedLocales = ['en', 'fr', 'pl', 'de'];
 
     /**
      * --------------------------------------------------------------------------
