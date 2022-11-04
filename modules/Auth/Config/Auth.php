379b9be2b99574fe4af4009b01128dba2c75f037
Yassine Doghri <yassine@doghri.fr>
2022-11-04 10:19:52 +0000

    fix(auth): disallow registration by default

--- modules/Auth/Config/Auth.php
+++ modules/Auth/Config/Auth.php
@@ -74,7 +74,7 @@ class Auth extends ShieldAuth
      * --------------------------------------------------------------------
      * Determines whether users can register for the site.
      */
-    public bool $allowRegistration = true;
+    public bool $allowRegistration = false;
 
     /**
      * --------------------------------------------------------------------
