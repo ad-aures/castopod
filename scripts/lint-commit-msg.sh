9ef58808fc473343d06ad271e2cee693732f3d25
Yassine Doghri <yassine@doghri.fr>
2023-02-22 17:25:14 +0000

    ci: update bundle scripts to use `pnpm exec` + add openssh-client to ci docker image

--- scripts/lint-commit-msg.sh
+++ scripts/lint-commit-msg.sh
@@ -6,14 +6,14 @@ set -e
 if [ "${CI_COMMIT_BEFORE_SHA}" = "0000000000000000000000000000000000000000" ];
 then
     echo "commitlint from HEAD^"
-    pnpx commitlint --from=HEAD^
+    pnpm exec commitlint --from=HEAD^
 else
     echo "commitlint from ${CI_COMMIT_BEFORE_SHA}"
     br=`git branch -r --contains ${CI_COMMIT_BEFORE_SHA}`
     if [ ! -n $br ];
     then 
-        pnpx commitlint --from=HEAD^
+        pnpm exec commitlint --from=HEAD^
     else
-        pnpx commitlint --from="${CI_COMMIT_BEFORE_SHA}"
+        pnpm exec commitlint --from="${CI_COMMIT_BEFORE_SHA}"
     fi
 fi
