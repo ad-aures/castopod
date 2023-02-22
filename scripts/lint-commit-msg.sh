e0c3ddb07d129b8723c558a99bef4cefb508a62b
Yassine Doghri <yassine@doghri.fr>
2023-02-22 16:59:28 +0000

    ci: use pnpx in lint-commit-msg script

--- scripts/lint-commit-msg.sh
+++ scripts/lint-commit-msg.sh
@@ -6,14 +6,14 @@ set -e
 if [ "${CI_COMMIT_BEFORE_SHA}" = "0000000000000000000000000000000000000000" ];
 then
     echo "commitlint from HEAD^"
-    npx commitlint --from=HEAD^
+    pnpx commitlint --from=HEAD^
 else
     echo "commitlint from ${CI_COMMIT_BEFORE_SHA}"
     br=`git branch -r --contains ${CI_COMMIT_BEFORE_SHA}`
     if [ ! -n $br ];
     then 
-        npx commitlint --from=HEAD^
+        pnpx commitlint --from=HEAD^
     else
-        npx commitlint --from="${CI_COMMIT_BEFORE_SHA}"
+        pnpx commitlint --from="${CI_COMMIT_BEFORE_SHA}"
     fi
 fi
