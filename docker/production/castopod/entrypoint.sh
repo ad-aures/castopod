04b2d8bafa77e03574490656075a95831da53451
Romain de Laage <romain@delaage.fr>
2023-10-23 11:04:51 +0000

    build(docker): update nginx unit image to 1.31.0

--- docker/production/castopod/entrypoint.sh
+++ docker/production/castopod/entrypoint.sh
@@ -3,8 +3,6 @@
 ENV_FILE_LOCATION=/var/www/castopod/.env
 
 . /prepare_environment.sh
-cat /config.template.json | envsubst '$CP_MAX_BODY_SIZE_BYTES$CP_TIMEOUT' > /config.json
+cat /config.template.json | envsubst '$CP_MAX_BODY_SIZE_BYTES$CP_TIMEOUT' > /usr/local/var/lib/unit/conf.json
 
-#Apply configuration after unit is started
-(sleep 2 && curl -X PUT --data-binary @/config.json --unix-socket /var/run/control.unit.sock http://localhost/config/) &
 supervisord
