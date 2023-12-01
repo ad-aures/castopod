402e93da4a0507105e9a4da85e2dd29459f8567a
Hue <tanguy.hue@etu.univ-nantes.fr>
2023-12-01 17:22:40 +0000

    feat: ajout phrase pour sondage

--- app/Language/fr/Home.php
+++ app/Language/fr/Home.php
@@ -17,4 +17,14 @@ return [
         'created_asc' => 'Le plus ancien d’abord',
     ],
     'no_podcast' => 'Aucun podcast trouvé',
+    'survey' => [
+        'title' => 'Votre avis nous intéresse',
+        'button' => 'Donnez votre avis',
+        'ease_of_use' => 'Facilité d\'utilisation de la plateforme pour écouter des podcasts',
+        'content_quality' => 'Évaluation de la qualité globale du contenu sur la plateforme',
+        'audio_quality' => 'Évaluation de la qualité audio des podcasts sur la plateforme',
+        'variety_of_content' => 'Évaluation de la variété des genres et sujets disponibles sur la plateforme',
+        'recommendation_likelihood' => 'Probabilité de recommander la plateforme à d\'autres auditeurs',
+        'validate' => 'Valider',
+    ],
 ];
