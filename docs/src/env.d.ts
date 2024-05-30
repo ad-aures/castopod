281eefc6a30750758762fb51549fb0f8002c91c8
Yassine Doghri <yassine@doghri.fr>
2024-05-30 09:42:03 +0000

    build(docs): add type declarations for virtual:starlight

--- docs/src/env.d.ts
+++ docs/src/env.d.ts
@@ -1,3 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference path="../node_modules/@astrojs/starlight/virtual.d.ts"/>
