37ee6d35b4bb66ce23dc271fb846200d1be0e7f6
Yassine Doghri <yassine@doghri.fr>
2024-07-30 09:40:50 +0000

    fix(icons): set correct names for lock and lock-unlock icons in premium banner

--- themes/cp_app/podcast/_partials/premium_banner.php
+++ themes/cp_app/podcast/_partials/premium_banner.php
@@ -4,8 +4,10 @@ if ($podcast->is_premium): ?>
     <?php
         $isUnlocked = service('premium_podcasts')
             ->isUnlocked($podcast->handle);
-    $shownIcon = $isUnlocked ? 'lock-unlock' : 'lock';
-    $hiddenIcon = $isUnlocked ? 'lock' : 'lock-unlock';
+    // @icon('lock-unlock-fill')
+    // @icon('lock-fill')
+    $shownIcon = $isUnlocked ? 'lock-unlock-fill' : 'lock-fill';
+    $hiddenIcon = $isUnlocked ? 'lock-fill' : 'lock-unlock-fill';
     ?>
     <div class="flex flex-col items-center justify-between col-start-2 px-2 py-1 mt-2 sm:px-1 md:mt-4 rounded-conditional-full gap-y-2 sm:flex-row bg-accent-base gap-x-2 text-accent-contrast">
         <p class="inline-flex items-center text-sm md:pl-4 gap-x-2"><?= $isUnlocked ? lang('PremiumPodcasts.banner_lock') : lang('PremiumPodcasts.banner_unlock') ?></p>
