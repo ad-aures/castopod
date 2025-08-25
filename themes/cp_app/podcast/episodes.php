f88abd26c8d411f8b8957fc8b26fca1f8084e3c3
Yassine Doghri <yassine@doghri.fr>
2025-08-25 16:46:45 +0000

    fix(episodes): set dropdown menu for seasons / years to a maximum height with auto scroll

--- themes/cp_app/podcast/episodes.php
+++ themes/cp_app/podcast/episodes.php
@@ -23,7 +23,7 @@
                     'class' => 'ml-2 text-xl',
                 ]) ?>
             </button>
-            <nav id="episode-lists-dropdown-menu" class="flex flex-col py-2 rounded-lg shadow border-3 border-contrast bg-elevated" aria-labelledby="episode-lists-dropdown" data-dropdown="menu" data-dropdown-placement="bottom-end">
+            <nav id="episode-lists-dropdown-menu" class="flex flex-col py-2 rounded-lg shadow border-3 border-contrast bg-elevated max-h-48 overflow-y-auto" aria-labelledby="episode-lists-dropdown" data-dropdown="menu" data-dropdown-placement="bottom-end">
                 <?php foreach ($episodesNav as $link): ?>
                     <?= anchor(
                         $link['route'],
@@ -42,8 +42,8 @@
     <div class="flex flex-col mt-4 gap-y-4">
         <?php foreach ($episodes as $episode): ?>
             <?= view('episode/_partials/card', [
-                    'episode' => $episode,
-                    'podcast' => $podcast,
+                'episode' => $episode,
+                'podcast' => $podcast,
             ]) ?>
         <?php endforeach; ?>
     </div>
